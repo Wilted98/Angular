3fcdc70af2c98de9bf8bb8d53907047f80502b2a
vasi <vasy.kr7@gmail.com>
2022-09-08 21:51:16 +0300

    Make a blueprint.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+type T = {
+  type: string;
+  name: string;
+  content: string;
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'LearnAng14';
+  serverElements: Array<T> = [];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBluePrintAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
 }
